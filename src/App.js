b34c797c2f4775cbb0975330bf0a89b36a6753b5
pranavc1515 <106530024+pranavc1515@users.noreply.github.com>
2023-12-21 00:18:48 +0530

    changes cretaed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+  const [searchInfo, setSearchInfo] = useState({});
+  const handleSearch = async (e) => {
+    e.preventDefault();
+    if (search === "") return;
+    const endpoint = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info&inprop=url&utf8=&format=json&origin=*&srlimit=20&srsearch=${search}`;
+    const response = await fetch(endpoint);
+    console.log(response);
+
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+
+    const json = await response.json();
+
+    setResults(json.query.search);
+    setSearchInfo(json.query.searchinfo);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>TinyWiki</h1>
+        <form className="search-box" onSubmit={handleSearch}>
+          <input
+            type="search"
+            placeholder="what are you looking for"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+        </form>
+        {searchInfo.totalhits ? (
+          <p>Search Results : {searchInfo.totalhits}</p>
+        ) : (
+          ""
+        )}
       </header>
+      <div className="results">
+        {results.map((results, i) => {
+
+          const url = `https://en.wikipedia.org/?curid=${results.pageid}`;
+
+
+          return (
+            <div className="results" key={i}>
+              <h3>{results.title}</h3>
+              <p dangerouslySetInnerHTML={{ __html:results.snippet}}>
+               
+              </p>
+              <a href={url} target="_blank" rel="noreferrer"> Read more</a>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
 export default App;
